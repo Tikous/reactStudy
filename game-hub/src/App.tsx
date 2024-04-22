1371626d208b150b4f59ed68965f0ceaab10cc61
tikous <1120531060@qq.com>
2024-04-22 10:40:18 +0800

    Sort the games

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -11,13 +11,11 @@ import SortSelector from "./components/SortSelector"
 export interface GameQuery {
   genre: Genre | null
   platform: Platform | null
+  sortOrder: string
 }
 
 function App() {
   const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery)
-
-  // const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null)
-  // const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(null)
 
   return (
     <Grid
@@ -42,7 +40,7 @@ function App() {
       <GridItem area='main'>
         <HStack spacing={5} paddingLeft={2} marginBottom={5}>
           <PlatformSelector selectedPlatform={gameQuery.platform} onSelectedPlatform={(platform) =>setGameQuery({ ...gameQuery, platform}) } />
-          <SortSelector />
+          <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder) => setGameQuery({ ...gameQuery, sortOrder })} />
         </HStack>
         <GameGrid gameQuery={gameQuery} />
       </GridItem>
