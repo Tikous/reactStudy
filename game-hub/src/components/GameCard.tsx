00c51253fefbe877892c7b06c219a0aeafa3b333
Tikous <1120531060@qq.com>
2024-05-27 21:01:27 +0800

    Refactor- move entity interfaces

--- game-hub/src/components/GameCard.tsx
+++ game-hub/src/components/GameCard.tsx
@@ -1,4 +1,4 @@
-import { Game } from '../hooks/useGames'
+import { Game } from "../entities/Game"
 import { Card, CardBody, HStack, Heading, Image, Text } from '@chakra-ui/react'
 import PlatFormIconList from './PlatformIconList'
 import CriticScore from './CriticScore'
